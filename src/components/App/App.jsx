5a33817a053a5dbb56d8d1e479c964c248baa503
more-v-kaple <morevkaple@gmail.com>
2018-07-23 22:51:27 +0300

    updated App component with Redux-provider

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+
+import configureStore from 'Redux/store/configureStore';
+
+const store = configureStore();
 
 import Main from './components/Main';
 
 const App = () => (
-    <Main/>
+    <Provider store = { store }>
+        <Main/>
+    </Provider>
 );
 
 export default App;
